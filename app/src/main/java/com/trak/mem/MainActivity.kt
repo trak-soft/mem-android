fbeeb23b71a02e9d4a4eb29722c343d038925574
Emmanuel Kareem <ekareem@finishline.com>
2022-01-22 18:16:48 -0500

    MEM-14 create blank pages and added navigation

--- app/src/main/java/com/trak/mem/MainActivity.kt
+++ app/src/main/java/com/trak/mem/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.trak.mem.scene.NavGraph
+import com.trak.mem.scene.NavGraphs
 import com.trak.mem.scene.landing.LandingScreen
 import com.trak.mem.scene.landing.LandingViewModel
 import com.trak.mem.ui.theme.MemandroidTheme
@@ -16,9 +19,7 @@ class MainActivity : ComponentActivity() {
             MemandroidTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LandingScreen(
-                        viewModel = LandingViewModel()
-                    )
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
