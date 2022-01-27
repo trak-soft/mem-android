48bad76cb1898be20933920ee64175ab13558287
ekareem <emmanuel45kareem@gmail.com>
2022-01-27 02:29:32 -0500

    MEM-14 added slide in animation to navigation transitions

--- app/src/main/java/com/trak/mem/scene/create/CreateScreen.kt
+++ app/src/main/java/com/trak/mem/scene/create/CreateScreen.kt
@@ -4,22 +4,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
 @Destination
 @Composable
 fun CreateScreen(
-    navigator: DestinationsNavigator
 ){
     Text(text = "create")
 }
 @Preview(showSystemUi = true)
 @Composable
 fun CreateScreenPreview(
-
 ){
-    CreateScreen(
-        EmptyDestinationsNavigator
-    )
+    CreateScreen()
 }
