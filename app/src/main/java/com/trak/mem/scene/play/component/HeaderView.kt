8548c08b43d266bf0e59a02c940a2af9f5547ee6
ekareem <emmanuel45kareem@gmail.com>
2022-01-29 07:49:02 -0500

    MEM-30 header view for play page

---
+++ app/src/main/java/com/trak/mem/scene/play/component/HeaderView.kt
@@ -0,0 +1,70 @@
+package com.trak.mem.scene.play.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.trak.mem.R
+import com.trak.mem.common.component.TextIconView
+
+@Composable
+fun HeaderView(
+    clickLimit: Int,
+    timeLimit: Int,
+    onReset: () -> Unit,
+    tint: Color,
+    modifier: Modifier = Modifier
+){
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        TextIconView(
+            clickLimit.toString(),
+            R.drawable.ic_click_limit,
+            stringResource(id = R.string.ic_click_limit_content_description),
+            fontSize = 32.sp,
+            iconSize = 32.dp,
+            tint = tint
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_reset),
+            contentDescription = stringResource(id = R.string.ic_add_game_content_description),
+            tint = tint,
+            modifier = Modifier.clickable {
+                onReset()
+            }
+        )
+        TextIconView(
+            timeLimit.toString(),
+            R.drawable.ic_time_limit,
+            stringResource(id = R.string.ic_time_limit_content_description),
+            fontSize = 32.sp,
+            iconSize = 32.dp,
+            tint = tint
+        )
+    }
+}
+
+@Preview()
+@Composable
+fun HeaderViewPreview(){
+        HeaderView(
+            23,
+            33,
+            {},
+            MaterialTheme.colors.onSurface,
+            modifier = Modifier.fillMaxWidth()
+        )
+}
